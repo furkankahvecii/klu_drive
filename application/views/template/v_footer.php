b352b581068ca63397177ff88e43e0e12a8bcaea
furkankahvecii <furkankahveci@hotmail.com.tr>
2020-03-30 12:08:40 +0300

    Update v_footer.php

--- application/views/template/v_footer.php
+++ application/views/template/v_footer.php
@@ -2,15 +2,15 @@
 </div>    
 </div>
 </div>   
-    <script type="text/javascript" src="<?php echo base_url(); ?>/html/files/bower_components/jquery/js/jquery.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>/html/files/bower_components/jquery-ui/js/jquery-ui.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>/html/files/bower_components/popper.js/js/popper.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>/html/files/bower_components/bootstrap/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>/html/files/assets/pages/waves/js/waves.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>/html/files/bower_components/jquery-slimscroll/js/jquery.slimscroll.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>/html/files/assets/js/pcoded.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>/html/files/assets/js/vertical/vertical-layout.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>/html/files/assets/js/script.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>html/files/bower_components/jquery/js/jquery.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>html/files/bower_components/jquery-ui/js/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>html/files/bower_components/popper.js/js/popper.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>html/files/bower_components/bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>html/files/assets/pages/waves/js/waves.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>html/files/bower_components/jquery-slimscroll/js/jquery.slimscroll.js"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>html/files/assets/js/pcoded.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>html/files/assets/js/vertical/vertical-layout.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>html/files/assets/js/script.min.js"></script>
      <?php //$this->load->view('js_manager'); ?> 
 </body>
 </html>
